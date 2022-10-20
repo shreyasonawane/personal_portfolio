dcb5beb830a4a6d8dd3da2d62411927578a597e8
Shreya Sonawane <94279051+shreyasonawane@users.noreply.github.com>
2022-10-20 10:52:52 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,8 @@
+<script type="text/javascript">
+            var image1=new Image()
+            image1.src="wepitch_cert.jpg"
+            var image2=new Image()
+            image2.src="Group 2.png"
+            var image3=new Image()
+            image3.src="Group 3.png"
+            </script>
